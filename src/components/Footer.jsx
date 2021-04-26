32f58dfef36691e518a715846d03354a903f3800
jdunk <jdunk@users.noreply.github.com>
2021-04-26 11:07:50 -0400

    Add footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,11 @@ import './Footer.scss';
 function Footer() {
   return (
     <div className="Footer">
-      App hand-coded by <strong>Jared Duncan</strong> using React
+      App hand-coded by <strong>Jared Duncan</strong>
+      {' using '}
+      <a href="https://reactjs.org/" target="_blank" rel="noreferrer">React</a>
+      {' and '}
+      <a href="https://material-ui.com/" target="_blank" rel="noreferrer">Material UI</a>
     </div>
   );
 }
