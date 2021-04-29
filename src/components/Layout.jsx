425071619add27aebe358f724ca370915baa2b72
jdunk <jdunk@users.noreply.github.com>
2021-04-29 13:19:29 -0400

    Update Layout: make toolbar height and gap size more consistent across breakpoints

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,4 @@
-import { useTheme } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -10,23 +9,19 @@ import logo from '../assets/img/logo.png';
 import './Layout.scss';
 
 function Layout() {
-  const theme = useTheme();
-
-  const mdAndUp = useMediaQuery(theme.breakpoints.up('md'));
-
   return (
     <>
       <AppBar>
-        <Toolbar {...(mdAndUp && {variant: 'dense'})}>
+        <Toolbar variant="dense">
           <Box display="block" mr={2} clone>
-            <img src={logo} width={40} height={40} />
+            <img src={logo} width={35} height={35} />
           </Box>
-          <h3 style={{ fontFamily: 'cursive' }}>Practice Spanish!</h3>
+          <Typography noWrap variant="h6" component="h1" style={{ fontFamily: 'cursive' }}>Practice Spanish!</Typography>
         </Toolbar>
       </AppBar>
 
       <Box className="Layout" display="flex" flexDirection="column">
-        <Box flexGrow={1} mt={{ xs: 7, md: 5, lg: 6 }}>
+        <Box flexGrow={1} mt={{ xs: 6 }}>
           <MainContent></MainContent>
         </Box>
         <Footer></Footer>
