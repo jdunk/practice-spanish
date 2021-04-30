fe31ac7366d56a7f28a7230c7ea5a599b01deb87
jdunk <jdunk@users.noreply.github.com>
2021-04-30 16:54:45 -0400

    Prettify the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,29 @@
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import Hidden from '@material-ui/core/Hidden';
+import bgColor from '@material-ui/core/colors/lightBlue';
+import { makeStyles } from '@material-ui/core/styles';
 import './Footer.scss';
 
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: bgColor[50],
+  }
+});
+
 function Footer() {
+  const classes = useStyles();
+
   return (
-    <div className="Footer">
-      App hand-coded by <strong>Jared Duncan</strong>
-      {' using '}
-      <a href="https://reactjs.org/" target="_blank" rel="noreferrer">React</a>
-      {' and '}
-      <a href="https://material-ui.com/" target="_blank" rel="noreferrer">Material UI</a>
-    </div>
+    <Box className={`Footer ${classes.root}`} px={1} boxShadow={4} mt={3}>
+      App hand-coded by <Link href="https://jaredduncan.com/" target="_blank"><strong>Jared Duncan</strong></Link>
+      <Hidden only="xs">
+        {' using '}
+        <Link href="https://reactjs.org/" target="_blank" rel="noreferrer">React</Link>
+        {' and '}
+        <Link href="https://material-ui.com/" target="_blank" rel="noreferrer">Material UI</Link>
+      </Hidden>
+    </Box>
   );
 }
 
